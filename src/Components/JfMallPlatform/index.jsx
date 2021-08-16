942115aade019be54a7905d9bfb63d3ccfc087f4
wuzhida01 <wuzhida01@corp.netease.com>
2021-08-16 12:11:33 +0800

    feat: 更换jfMallPlatform组件为hook组件

--- src/Components/JfMallPlatform/index.jsx
+++ src/Components/JfMallPlatform/index.jsx
@@ -1,35 +1,61 @@
-import React, {} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {jfFederationPath} from "../../utils/common";
 import {Spin} from "antd";
 
-class JfMallPlatform extends React.Component {
-  history = null;
-  state = {
-    loading: true
-  };
+// class JfMallPlatform extends React.Component {
+//   history = null;
+//   state = {
+//     loading: true
+//   };
+//
+//   componentDidMount() {
+//     import('jfPlatformUi/Base/StorePage').then(res => {
+//       this.history = res.default();
+//       this.setState({loading: false})
+//     })
+//   }
+//
+//   componentDidUpdate() {
+//     if (this.history) {
+//       this.history.push(window.location.pathname.replace(jfFederationPath, ''))
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <Spin spinning={this.state.loading} size="large" wrapperClassName="content">
+//         <div id="alert"/>
+//         <div id="confirm"/>
+//         <div id="content"/>
+//       </Spin>
+//     )
+//   }
+// }
 
-  componentDidMount() {
+const JfMallPlatform = () => {
+  const [loading, setLoading] = useState(true);
+  const history = useRef(null);
+
+  useEffect(() => {
     import('jfPlatformUi/Base/StorePage').then(res => {
-      this.history = res.default();
-      this.setState({loading: false})
+      history.current = res.default();
+      setLoading(false);
     })
-  }
+  }, []);
 
-  componentDidUpdate() {
-    if (this.history) {
-      this.history.push(window.location.pathname.replace(jfFederationPath, ''))
+  useEffect(() => {
+    if (history.current) {
+      history.current.push(window.location.pathname.replace(jfFederationPath, ''))
     }
-  }
+  });
 
-  render() {
-    return (
-      <Spin spinning={this.state.loading} size="large" wrapperClassName="content">
-        <div id="alert"/>
-        <div id="confirm"/>
-        <div id="content"/>
-      </Spin>
-    )
-  }
+  return (
+    <Spin spinning={loading} size="large" wrapperClassName="content">
+      <div id="alert"/>
+      <div id="confirm"/>
+      <div id="content"/>
+    </Spin>
+  )
 }
 
 export default JfMallPlatform
